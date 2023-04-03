a5a40046312b1009b2d762ec50a22c6e28cdbe21
Kenneth, Cho Kai Hung <107766043+KennethCho-CKH@users.noreply.github.com>
2023-04-03 14:00:42 +0100

    Enhance Consistency

--- response.go
+++ response.go
@@ -1,23 +1,23 @@
 package slack
 
-// Response is a generic response from slack which implements SendResponse.
+// Response is a generic response from Slack which implements the SendResponse interface.
 type Response struct {
 	OK      bool   `json:"ok"`
 	Error   string `json:"error,omitempty"`
 	Warning string `json:"warning,omitempty"`
 }
 
-// Ok implements SendResponse.Ok.
+// Ok implements the SendResponse.Ok method.
 func (r Response) Ok() bool {
 	return r.OK
 }
 
-// Err implements SendResponse.Err.
+// Err implements the SendResponse.Err method.
 func (r Response) Err() string {
 	return r.Error
 }
 
-// Warn implements SendResponse.Warn.
+// Warn implements the SendResponse.Warn method.
 func (r Response) Warn() string {
 	return r.Warning
 }
